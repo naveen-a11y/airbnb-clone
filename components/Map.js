ba6c0bebffd78fad12e60168a1e19c905e38c455
Naveen Rawat <naveenrawat793@gmail.com>
2021-09-14 20:48:20 +0530

    final commit

---
+++ components/Map.js
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import { getCenter } from "geolib";
+
+function Map({ searchResults }) {
+  const [selectedLocation, setSelectedLocation] = useState({});
+
+  const cordinates = searchResults.map((result) => ({
+    longitude: result.long,
+    latitude: result.lat,
+  }));
+
+  const center = getCenter(cordinates);
+
+  const [viewport, setViewPort] = useState({
+    width: "100%",
+    height: "100%",
+    latitude: center.latitude,
+    longitude: center.longitude,
+    zoom: 11,
+  });
+
+  return (
+    <ReactMapGL
+      mapStyle="mapbox://styles/rawatnaveen/cktis1tuo6han17ppvcahc3yv"
+      mapboxApiAccessToken={process.env.mapbox_key}
+      {...viewport}
+      onViewportChange={(nextViewport) => setViewPort(nextViewport)}
+    >
+      {searchResults.map((result) => (
+        <div key={result.long}>
+          <Marker
+            longitude={result.long}
+            latitude={result.lat}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <p
+              role="img"
+              onClick={() => setSelectedLocation(result)}
+              className="cursor-pointer text-xl animate-bounce"
+              aria-label="push-pin"
+            >
+              🚀
+            </p>
+          </Marker>
+          {selectedLocation.long === result.long ? (
+            <Popup
+              onClose={() => setSelectedLocation({})}
+              onClick={true}
+              latitude={result.lat}
+              longitude={result.long}
+            >
+              {result.title}
+            </Popup>
+          ) : (
+            false
+          )}
+        </div>
+      ))}
+    </ReactMapGL>
+  );
+}
+
+export default Map;
--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoicmF3YXRuYXZlZW4iLCJhIjoiY2t0aXJ4a2VuMTU4NTJ1bnU2YzA3MGpzdiJ9.5ZkX_uuQEo8oJlnhvBIgWA",
+  },
 };
